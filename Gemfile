3e9ae777b29a7fee0ad17bdb13db0d9e58099ea6
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-30 13:53:31 -0400

    after lunch push

--- Gemfile
+++ Gemfile
@@ -38,3 +38,5 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+# Added by me to run on work winblows
+gem 'tzinfo-data'
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+
+# Controller for my blog
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  # def create
+  #   render plain: params[:article].inspect
+  # end
+end
